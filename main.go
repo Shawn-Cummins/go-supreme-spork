461e4957823ec47c0c0fab913d6a76fce0a7a7ef
Kevin Platt <Kevin.Platt@dcsg.com>
2025-02-25 17:12:50 +0000

    cleanup

--- main.go
+++ main.go
@@ -2,49 +2,15 @@ package main
 
 import (
 	_ "embed"
-	"encoding/json"
-	"fmt"
 	"log"
 	"net/http"
-	"supreme-spork/models"
+	"supreme-spork/internal/handlers"
 )
-
-type Response struct {
-	Message string      `json:"message"`
-	Data    interface{} `json:"data"`
-}
-
-type Config struct {
-	Port int    `json:"port"`
-	Host string `json:"host"`
-}
-
-// GenericMap is a map with string keys and values of any type T.
-type GenericMap[T any] map[string]T
 
 //go:embed api/us.json
 var usJson []byte
 
-
 func main() {
-	http.HandleFunc("/foo", fooHandler)
+	http.HandleFunc("/foo", handlers.FooHandler(usJson))
 	log.Fatal(http.ListenAndServe(":8080", nil))
-}
-
-func fooHandler(w http.ResponseWriter, r *http.Request) {
-	queryParams := r.URL.Query()
-	log.Println(queryParams)
-
-	var payload models.Payload
-	if err := json.Unmarshal(usJson, &payload); err != nil {
-		http.Error(w, "Failed to parse JSON result test", http.StatusInternalServerError)
-		fmt.Println(err)
-		return
-	}
-
-	totalAreaText := payload.Geography.Coastline.Text
-    fmt.Println("Geography.Area.total.text:", totalAreaText)
-
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(payload.People.DependencyRatios)
 }
